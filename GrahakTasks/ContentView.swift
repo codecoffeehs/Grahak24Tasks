4256538f377c985aa19af6273e44ec0b431dcfc3
Hemant Sharma <codecoffeehs@icloud.com>
2026-01-06 09:54:51 +0530

    Initial Commit

---
+++ GrahakTasks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GrahakTasks
+//
+//  Created by Hemant Sharma on 06/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GrahakTasks/GrahakTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrahakTasksApp.swift
+//  GrahakTasks
+//
+//  Created by Hemant Sharma on 06/01/26.
+//
+
+import SwiftUI
+
+@main
+struct GrahakTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
